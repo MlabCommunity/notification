57ac626668a6c75a64b1ea4ee4ce3991a71e639a
Adrian Nieckarz <adrian.nieckarz@outlook.com>
2022-07-26 15:04:24 +0200

    Core refactor, Added communication with identity service

--- Lappka.Notification.Application/Services/NotificationGrpcService.cs
+++ Lappka.Notification.Application/Services/NotificationGrpcService.cs
@@ -1,15 +1,25 @@
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
-using Microsoft.AspNetCore.Identity;
 
 namespace Lappka.Notification.Application.Services;
 
 public class NotificationGrpcService : NotificationService.NotificationServiceBase
 {
-    public override async Task<Empty> RestartPassword(RestartPasswordRequest request, ServerCallContext context)
+    public override async Task<Empty> ConfirmEmail(ConfirmEmailRequest request, ServerCallContext context)
     {
-        Console.WriteLine($"User with email: {request.Email} requested password reset");
+        Console.WriteLine($"ConfirmEmail called,Email {request.Email}, token {request.Token}");
+        return new();
+    }
 
+    public override async Task<Empty> ResetEmail(ResetEmailRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"ResetEmail called,Email {request.Email}, token {request.Token}");
+        return new();
+    }
+
+    public override async Task<Empty> RestartPassword(ResetPasswordRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"RestartPassword called,Email {request.Email}, token {request.Token}");
         return new();
     }
 }
