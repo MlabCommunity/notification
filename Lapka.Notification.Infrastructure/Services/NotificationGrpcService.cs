8d8808b3ba6222c54b2f89787c74c7647ac67662
Madziks <majta.magdalena@gmail.com>
2022-07-27 11:34:21 +0200

    gRPC primary

--- Lapka.Notification.Infrastructure/Services/NotificationGrpcService.cs
+++ Lapka.Notification.Infrastructure/Services/NotificationGrpcService.cs
@@ -5,10 +5,21 @@ namespace Lapka.Notification.Infrastructure.Services;
 
 public class NotificationGrpcService : NotificationService.NotificationServiceBase
 {
-    public override Task<Empty> RestartPassword(RestartPasswordRequest request, ServerCallContext context)
+    public override Task<Empty> ResetPassword(ResetPasswordRequest request, ServerCallContext context)
     {
         Console.WriteLine($"Reset hasła dla emaila {request.Email}, tokenem: {request.Token}");
-        return Task.FromResult<Empty>(null);
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> ResetEmail(ResetEmailRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"Reset maila {request.Email}, tokenem: {request.Token}");
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> ConfirmEmail(ConfirmEmailRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"Potwierdzenie maila {request.Email}, tokenem: {request.Token}");
+        return Task.FromResult(new Empty());
     }
 }
-
